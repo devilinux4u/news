b1f56f7afb937a313ae45e3fa2e85f9aaa27bf67
sololinux <chauraj345r@gmail.com>
2024-12-23 18:18:40 +0545

    db-fix

--- db/sequelize.js
+++ db/sequelize.js
@@ -7,17 +7,19 @@ const user = model.user(sequelize, DataTypes);
 const info = model.info(sequelize, DataTypes);
 
 
-try {
-    sequelize.sync();
-    console.log('Database synced succefully');
-    const userCount = await user.count();
-    if (userCount === 0) {
-        await user.create({ uname: 'admin', pass: 'admin' });
-        console.log('Default admin user created with uname: admin and pass: admin');
+(async () => {
+    try {
+        sequelize.sync();
+        console.log('Database synced succefully');
+        const userCount = await user.count();
+        if (userCount === 0) {
+            await user.create({ uname: 'admin', pass: 'admin' });
+            console.log('Default admin user created with uname: admin and pass: admin');
+        }
+    } catch (error) {
+        console.error('Unable to connect to the database:\n', error);
     }
-} catch (error) {
-    console.error('Unable to connect to the database:\n', error);
-}
+})();
 
 
 module.exports = {
