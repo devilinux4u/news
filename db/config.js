044b4943c65350ecae54035bddbe8fa278e36f83
sololinux <chauraj345r@gmail.com>
2024-12-23 17:28:57 +0545

    db

--- db/config.js
+++ db/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     db_con: {
-        username: 'root',
-        password: '',
-        database: 'news',
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3307
+        username: 'news_db_5if8_user', //'root',
+        password: 'xWRh7lzOs1nctcPbXFEogU1vC5APZP7G',
+        database: 'news_db_5if8', //'news',
+        host: 'dpg-ctkko7aj1k6c73cpjllg-a', //'localhost',
+        dialect: 'postgresql',
+        port: 5432
     },
 };
